c6f9372fda2fbf1c79cc8ff75afb47c186a72a10
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-17 18:42:00 -0400

    Add Exc 28 - 29 part01

--- Part 01/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
+++ Part 01/part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -5,7 +5,16 @@ public class Adulthood {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("How old are you?");
+        int edad = scan.nextInt();
+        if (edad >= 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are not an adult");
+        }
 
-        // Write your program here 
+        
     }
 }
+
+
