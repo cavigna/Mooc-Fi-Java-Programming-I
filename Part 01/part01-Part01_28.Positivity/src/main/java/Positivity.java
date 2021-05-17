43a4161f66b22d323f13f8babda7207eee984602
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-17 18:35:28 -0400

    Added Exc 27 -28

--- Part 01/part01-Part01_28.Positivity/src/main/java/Positivity.java
+++ Part 01/part01-Part01_28.Positivity/src/main/java/Positivity.java
@@ -5,6 +5,13 @@ public class Positivity {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int n = scan.nextInt();
+        if (n > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
 
         // Write your program here
     }
