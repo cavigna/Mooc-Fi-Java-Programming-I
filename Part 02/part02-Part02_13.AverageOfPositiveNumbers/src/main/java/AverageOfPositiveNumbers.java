fd1d42a8949ea657ec31ca1c87501186015c1870
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-17 23:34:12 -0400

    Added Exc 13 part02

--- Part 02/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Part 02/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -6,5 +6,27 @@ public class AverageOfPositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int number = scanner.nextInt();
+            if (number == 0) {
+                break;
+            } else if (number > 0) {
+                counter += 1;
+                sum += number;
+
+            }
+
+        }
+        double result = 1.0 * sum / counter;
+        if (result > 0) {
+            System.out.println("Average of the numbers: " + result);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+
     }
 }
