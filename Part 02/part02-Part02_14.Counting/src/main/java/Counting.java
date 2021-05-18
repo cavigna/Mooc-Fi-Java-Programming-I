fe41b50d547e07f74359f66b4b35855f088f9075
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-18 14:56:10 -0400

    Exc 14-15 part02

--- Part 02/part02-Part02_14.Counting/src/main/java/Counting.java
+++ Part 02/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,11 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //System.out.println("Give number: ");
+        int num = scanner.nextInt();
+        for(int i=0; i<=num; i++){
+            System.out.println(i);
+        }
 
     }
 }
