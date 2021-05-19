6d2509fdd93caf4d6237493c93fbb24429ed8480
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-19 18:45:41 -0400

    Added 21-22 part03

---
+++ Part 03/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -0,0 +1,31 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+        public static void printArrayInStars(int[] array) {
+        // Write some code in here
+        for (int i = 0; i < array.length; i++){
+            int val = array[i];
+            for (int j = 0; j < val; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+    
+    
+    
+    /*public static void printArrayInStars(int[] array) {
+        String s ="*";
+        for(int i:array){
+            System.out.println(s.repeat(i));
+        }
+    }*/
+
+}
