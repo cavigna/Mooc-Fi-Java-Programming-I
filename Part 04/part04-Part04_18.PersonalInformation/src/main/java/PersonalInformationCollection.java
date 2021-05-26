49c02b3a90ce574c955ba9b6f66f5ef3355e80a9
cavigna <40524472+cavigna@users.noreply.github.com>
2021-05-26 16:06:10 -0400

    18 part04

--- Part 04/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ Part 04/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -10,5 +10,27 @@ public class PersonalInformationCollection {
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            System.out.println("First name: ");
+            String nombre = scanner.nextLine();
+
+            if(nombre.isEmpty()){
+                break;
+            }
+            System.out.println("Last name: ");
+            String apellido = scanner.nextLine();
+
+            System.out.println("Identification number: ");
+            String id = scanner.nextLine();
+
+
+            infoCollection.add(new PersonalInformation(nombre, apellido, id));
+        }
+
+
+        for (PersonalInformation info : infoCollection){
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
+
     }
 }
